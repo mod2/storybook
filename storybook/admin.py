9cb12c3c587320db652ecf712f3c83a82850f23b
bencrowder <ben.crowder@gmail.com>
2016-06-11 12:18:46 -0600

    Limits scene list in Django admin to story

--- storybook/admin.py
+++ storybook/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django import forms
+
 from .models import Story, Scene, Revision, Draft, Fragment, Character
 
 @admin.register(Story)
@@ -26,6 +28,21 @@ class FragmentAdmin(admin.ModelAdmin):
     list_display = ('story', 'created')
 
 
+class CharacterAdminForm(forms.ModelForm):
+    class Meta:
+        model = Character
+        fields = [ 'name', 'color', 'story', 'scenes', ]
+
+    def __init__(self, *args, **kwargs):
+        super(CharacterAdminForm, self).__init__(*args, **kwargs)
+
+        story = Story.objects.get(id=self.initial['story'])
+
+        self.fields['scenes'].queryset = Scene.objects.filter(story=story)
+
+
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
+    form = CharacterAdminForm
     list_display = ('name', 'color', 'story')
+    filter_horizontal = ('scenes',)
