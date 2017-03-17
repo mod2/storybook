d591d5b252f6d24a0d087db691655eb6dae61caa
bencrowder <ben.crowder@gmail.com>
2017-03-17 12:10:04 -0600

    Updates requirements and adds flat-responsive

--- storybook/settings.py
+++ storybook/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Application definition
 
 INSTALLED_APPS = (
+    'flat_responsive',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
